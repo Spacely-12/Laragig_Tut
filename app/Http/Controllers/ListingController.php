dfc29716c4823641c93107d9ee95e87740c693d7
Salawudeen <mohammedsalawudeen99@gmail.com>
2024-08-27 10:35:12 +0000

    Refactor pagination views and add pagination to listings index page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -18,7 +18,8 @@ class ListingController extends Controller
         return view(
             'listings.index',
             [
-                'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+                'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(10)
+                                                                                    //simplePaginate
             ]
         );
     }
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -19,4 +19,5 @@
             @endunless
         </div>
     </div>
+    <div class="mt-6 p-4">{{$listings -> links()}}</div>
 </x-layout>
